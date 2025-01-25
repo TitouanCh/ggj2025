14ecd1429f9ae6e3f7c0b2df1905e9f7929b403b
TitouanCh <titouan85000@gmail.com>
2025-01-25 12:46:45 +0100

    implentation minijeu

--- minigame/minigame.gd
+++ minigame/minigame.gd
@@ -1,11 +1,5 @@
+class_name Minigame
 extends Node2D
-signal isDead
+signal finished
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+const dimensions = Vector2(720, 405)
--- minigame/support.gd
+++ minigame/support.gd
@@ -1,6 +1,8 @@
 class_name Support
 extends SubViewportContainer
 
+signal finished
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -10,14 +12,15 @@ func _process(delta: float) -> void:
 	pass
 
 
+func _on_minigame_finished():
+	finished.emit()
+	self.queue_free()
+
 static func spawn_support(game: String) -> Support:
 	var support = load("res://minigame/support.tscn").instantiate()
-	print("res://minigame/"+game+".tscn")
-	var minigame: PackedScene = load("res://minigame/"+game+".tscn")
+	print("res://minigame/" + game + "/" + game + ".tscn")
+	var minigame: PackedScene = load("res://minigame/" + game + "/" + game + ".tscn")
 	var minigame_instance = minigame.instantiate()
-	
+	minigame_instance.finished.connect(support._on_minigame_finished)
 	support.add_child(minigame_instance)
 	return support
-	
-	
-	
