7c6b42759dc99e48d34649c8534e1fee13553009
MaiDeme <maiwen.demeulle@gmail.com>
2025-01-26 11:42:10 +0100

    random machine textures

--- game/machine.gd
+++ game/machine.gd
@@ -3,6 +3,30 @@ extends Interactable
 
 var true_name_task = "PlayMoney"
 var used = false
+var model = {
+	'green':[
+		load("res://model/machine/machine_base_green.tres"),
+		load("res://model/machine/machine_door_green.tres"),
+		load("res://model/machine/machine_logo_green.tres")
+	],
+	'yellow':[
+		load("res://model/machine/machine_base_yellow.tres"),
+		load("res://model/machine/machine_door_yellow.tres"),
+		load("res://model/machine/machine_logo_yellow.tres")
+	],
+	'red':[
+		load("res://model/machine/machine_base_red.tres"),
+		load("res://model/machine/machine_door_red.tres"),
+		load("res://model/machine/machine_logo_red.tres")
+	],
+	'purple':[
+		load("res://model/machine/machine_base_purple.tres"),
+		load("res://model/machine/machine_door_purple.tres"),
+		load("res://model/machine/machine_logo_purple.tres")
+	]
+}
+
+var model_keys=["green","yellow","red","purple"]
 
 func interact(player: Player):
 	print("Machine Soda")
@@ -21,3 +45,13 @@ func open():
 func close():
 	var tween = create_tween()
 	tween.tween_property($DoorJoint, "rotation:y", 0, 1.2).set_ease(Tween.EASE_IN_OUT)
+
+func _ready() -> void:
+	randomize()
+	var color = model_keys.pick_random()
+	$AssetMachineV2.mesh = model[color][0]
+	$DoorJoint/AssetMachinePorteV1.mesh = model[color][1]
+	$DoorJoint/AssetMachinePorteV1/logo.mesh = model[color][2]
+	
+	
+	
